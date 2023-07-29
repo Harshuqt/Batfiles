48f305004e31d2422c1f1dcdfddb99ad47e41398
Harshal Machhi <Harshal2040+dev@gmail.com>
2023-07-29 17:07:34 +0530

    Added Clean All Bat Files

---
+++ Clean_All/Clean_All.bat
@@ -0,0 +1,16 @@
+RD /S /Q "C:\Users\%Username%\AppData\Local\Temp"
+RD /S /Q "C:\Windows\SoftwareDistribution\Download"
+RD /S /Q "C:\Windows\Prefetch"
+RD /S /Q "C:\Windows\temp"
+RD /S /Q "C:\Windows\System32\sru\SRUDB.dat"
+RD /S /Q "C:\Users\%Username%\AppData\Local\NVIDIA\GLCache"
+RD /S /Q "C:\Users\%Username%\AppData\Local\Spotify\Data"
+RD /S /Q "C:\Users\%Username%\Desktop\debug.log"
+RD /S /Q "C:\Users\%Username%\AppData\Local\Packages\Microsoft.Windows.Search_cw5n1h2txyewy\LocalState\AppIconCache"
+RD /S /Q "C:\Users\%Username%\AppData\Local\AMD\CN"
+call CleanForza4.bat
+netsh winsock reset
+netsh int ip reset
+ipconfig /flushdns
+::ipconfig /release
+::ipconfig /renew
